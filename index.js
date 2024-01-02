296f5364d68acf5f3c608a61e05dea1f1095938a
Jaeem Ekram <jekram98@gmail.com>
2024-01-02 12:51:31 -0500

    changes made

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+fetch ("https://api.disneyapi.dev/character")
+.then((res) => res.json())
+.then ((data) => {
+console.log(data.data)
+})
 
-    fetch ("https://api.disneyapi.dev/character")
-    .then((res) => res.json())
-    .then ((data) => {
-    console.log(data)
-    })
+const renderCharacters = (char) => {
+    
+    let name = document.querySelector("#name")
+    name.textContent = char.name
+    
+    let film = document.querySelector("#film")
+    film.textContent = char.films
+    
+    let img = document.querySelector("#random-picture")
+    img.src = char.imageUrl
+
+    let show = document.querySelector("#show")
+    show.textContent = char.shows
+
+    }
+
+
     
 
