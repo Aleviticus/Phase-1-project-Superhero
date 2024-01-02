77815eaa31ea0eda3d1c044c84616764447997c7
Joseph Lee <jslee5413@gmail.com>
2024-01-02 05:16:56 -0500

    added form

--- index.js
+++ index.js
@@ -6,3 +6,19 @@
     })
     
 
+let form = document.getElementById("new-character");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    let newCharacter = {
+        name: e.target.name.value,
+        imageUrl: e.target.image.value,
+        films: e.target.films.value,
+        shortFilms: e.target["short-films"].value,
+        tvShows: e.target["tv-shows"].value,
+        videoGames: e.target["video-games"].value,
+    }
+
+    renderCharacter(newCharacter);
+})
