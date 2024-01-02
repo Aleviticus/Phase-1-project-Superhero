65ef52be7e94003a2d3db5998c9686d76d64c257
Aaron Levy <aaronlevypc21@gmail.com>
2024-01-02 17:24:28 -0500

    workingcode

--- index.js
+++ index.js
@@ -20,38 +20,38 @@ const renderCharacters = (char) => {
     let show = document.querySelector("#show")
     show.textContent = char.shows
 
-    }
-
-
-    
-
-let form = document.getElementById("new-character");
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-    let newCharacter = {
-        name: e.target.name.value,
-        imageUrl: e.target.image.value,
-        films: e.target.films.value,
-        shortFilms: e.target["short-films"].value,
-        tvShows: e.target["tv-shows"].value,
-        videoGames: e.target["video-games"].value,
-    }
-
-    renderCharacters(newCharacter);
-})
+}
 
 let random = document.getElementById('random')
 
-random.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    const randomCharacter = characters[randomIndex] 
-    if(characters.length > 0) {
-    console.log(randomCharacter) 
-    }
-    renderCharacters(randomCharacter)
-})
+    
+    random.addEventListener('click', () => {
+        console.log(random)
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        const randomCharacter = characters[randomIndex] 
+        if(characters.length > 0) {
+        console.log(randomCharacter) 
+        }
+        renderCharacters(randomCharacter)
+    })
+
+// let form = document.getElementById("new-character");
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault()
+
+//     let newCharacter = {
+//         name: e.target.name.value,
+//         imageUrl: e.target.image.value,
+//         films: e.target.films.value,
+//         shortFilms: e.target["short-films"].value,
+//         tvShows: e.target["tv-shows"].value,
+//         videoGames: e.target["video-games"].value,
+//     }
+
+//     renderCharacters(newCharacter);
+// })
+
 
 
 
